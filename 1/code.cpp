b5fb2e60c8c35552892236c8bc3b85f47a90ea4e
Pranshu Gupta <pranshumg@gmail.com>
2025-12-09 02:46:15 +0530

    Problem 1

---
+++ 1/code.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr int n = 1000;
+
+void solve() {
+    int sum = 0;
+    for (int i = 1; i < n; ++i) {
+        if (!(i % 3) || !(i % 5)) {
+            sum += i;
+        }
+    }
+    cout << sum << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
