36f86a9f8a70f8632b2753a60e33f82a05c22af6
Pranshu Gupta <pranshumg@gmail.com>
2025-12-09 02:50:35 +0530

    Problem 2

---
+++ 2/code.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr int n = 4000000;
+
+void solve() {
+    int a = 0, b = 1, sum = a + b;
+    ll ans = 0;
+    while (sum <= n) {
+        if (!(sum & 1)) {
+            ans += sum;
+        }
+        a = b;
+        b = sum;
+        sum = a + b;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
