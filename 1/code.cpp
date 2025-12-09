ddd3cf84965eeaf2dcc45facfc791e64c2f97032
Pranshu Gupta <pranshumg@gmail.com>
2025-12-09 19:06:55 +0530

    Problem 1

--- 1/code.cpp
+++ 1/code.cpp
@@ -9,9 +9,8 @@ using u128 = unsigned __int128;
 
 #define all(x) (x).begin(), (x).end()
 
-constexpr int n = 1000;
-
 void solve() {
+    constexpr int n = 1000;
     int sum = 0;
     for (int i = 1; i < n; ++i) {
         if (!(i % 3) || !(i % 5)) {
